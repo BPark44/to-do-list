f02fb78774586e966abdadb3282b81561866985b
BPark44 <baileyparkerb@gmail.com>
2022-10-27 14:50:18 +1100

    Added delete function

--- app.js
+++ app.js
@@ -1,33 +1,56 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
-
-let items = ["Example"];
 
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-app.get("/", function(req, res){
-    
-let today = new Date();
+mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewURLParser: true});
 
-let options = {
- weekday: "long",
- day: "numeric",
- month: "long"
+const itemsSchema = {
+    name: String
 };
 
-let day= today.toLocaleDateString("en-US", options);
+const Item = mongoose.model(
+    "Item",
+    itemsSchema
+);
 
-
-res.render('list', {
-    listTitle: day,
-    newListItems: items
+const item1 = new Item({
+    name: "Welcome to your todolist."
 });
 
+const item2 = new Item({
+    name: "Hit the + button to add a new item."
+});
+
+const item3 = new Item({
+    name: "<-- Hit this delete an item."
+});
+
+const defaultItems =[item1, item2, item3];
+
+app.get("/", function(req, res){
+
+    Item.find({}, function(err, foundItems){
+
+    if (foundItems.length === 0){
+        Item.insertMany(defaultItems, function(err){
+            if (err) {
+                console.log(err);
+            } else {
+                console.log("Successfully saved default items to DB");
+            }
+        });
+        res.redirect("/");
+    } else {
+        res.render('list', { listTitle: "Today", newListItems: foundItems});
+    }
+    });
 });
 
 app.get("/about", function(req, res){
@@ -35,15 +58,41 @@ app.get("/about", function(req, res){
 });
 
 app.post("/", function(req, res){
-    let item = req.body.newItem;
 
-    items.push(item);
+    const itemName = req.body.newItem;
+
+    const item = new Item({
+        name: itemName
+    });
+
+    item.save();
+
+    res.redirect("/");
+
+});
+
+app.post("/delete", function(req, res){
+    const deletedItem = req.body.deleteButton;
+
+    Item.findByIdAndRemove(deletedItem, function(err){
+        if (!err) {
+            console.log("Deleted item.");
+        }
+    });
 
     res.redirect("/");
 });
-
 
 app.listen(3000, function(){
     console.log("server is running on port 3000");
 });
 
+// let today = new Date();
+
+// let options = {
+//  weekday: "long",
+//  day: "numeric",
+//  month: "long"
+// };
+
+// let day= today.toLocaleDateString("en-US", options);
